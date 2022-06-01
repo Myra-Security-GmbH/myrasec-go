f3344cb761cb823825765a09ebc21fe2336e81cc
Richard Prillwitz <richard.prillwitz@myrasecurity.com>
2022-06-01 16:32:06 +0200

    added tests;

--- domain_test.go
+++ domain_test.go
@@ -9,10 +9,12 @@ import (
 )
 
 func preCacheGetDomain(api *API) {
+	responseJSON := `{"error": false, "pageSize": 10, "page": 1, "count": 1, "data": [{"id": 1, "name": "example.com"}]}`
+
 	req, _ := http.NewRequest(http.MethodGet, "https://apiv2.myracloud.com/domains/1", nil)
 	resp := http.Response{
 		Status: strconv.Itoa(http.StatusOK),
-		Body:   ioutil.NopCloser(bytes.NewBufferString(`{"error": false, "pageSize": 10, "page": 1, "count": 1, "data": [{"id": 1, "name": "example.com"}]}`)),
+		Body:   ioutil.NopCloser(bytes.NewBufferString(responseJSON)),
 	}
 
 	res, _ := methods["getDomain"].ResponseDecodeFunc(&resp, methods["getDomain"])
@@ -20,7 +22,17 @@ func preCacheGetDomain(api *API) {
 }
 
 func preCacheListDomains(api *API) {
+
+	responseJSON := `{"error": false, "pageSize": 10, "page": 1, "count": 3, "data": [{"id": 1, "name": "example.com"}, {"id": 2, "name": "example2.com"}, {"id": 3, "name": "example3.com"}]}`
+
 	req, _ := http.NewRequest(http.MethodGet, "https://apiv2.myracloud.com/domains", nil)
+	resp := http.Response{
+		Status: strconv.Itoa(http.StatusOK),
+		Body:   ioutil.NopCloser(bytes.NewBufferString(responseJSON)),
+	}
+
+	res, _ := decodeDefaultResponse(&resp, methods["listDomains"])
+	api.cacheResponse(req, res)
 }
 
 //
@@ -29,6 +41,7 @@ func preCacheListDomains(api *API) {
 func preCacheDomainAPI() (*API, error) {
 	api, _ := New("abc123", "123abc")
 	api.EnableCaching()
+	api.SetCachingTTL(120)
 
 	preCacheGetDomain(api)
 	preCacheListDomains(api)
@@ -68,4 +81,7 @@ func TestListDomains(t *testing.T) {
 		t.Errorf("Expected not to get an error but got [%s]", err.Error())
 	}
 
+	if len(domains) != 3 {
+		t.Errorf("Expected to get [%d] domains but got [%d]", 3, len(domains))
+	}
 }
