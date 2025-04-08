5cb875d14961d7b2ff2a8ebe8bf2d611b1f6b17b
Richard Prillwitz <richard@prillwitz.io>
2025-04-08 14:45:12 +0200

    Return slice of CacheClear structs;

--- cacheclear.go
+++ cacheclear.go
@@ -1,18 +1,22 @@
 package myrasec
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"reflect"
 )
 
 // getCacheClearMethods returns cache clear related API calls
 func getCacheClearMethods() map[string]APIMethod {
 	return map[string]APIMethod{
 		"clearCache": {
-			Name:   "clearCache",
-			Action: "domain/%d/cache-clear",
-			Method: http.MethodPut,
-			Result: CacheClear{},
+			Name:               "clearCache",
+			Action:             "domain/%d/cache-clear",
+			Method:             http.MethodPut,
+			Result:             []CacheClear{},
+			ResponseDecodeFunc: decodeCacheClearResponse,
 		},
 	}
 }
@@ -25,7 +29,7 @@ type CacheClear struct {
 }
 
 // ClearCache ...
-func (api *API) ClearCache(cacheClear *CacheClear, domainId int) (*CacheClear, error) {
+func (api *API) ClearCache(cacheClear *CacheClear, domainId int) (*[]CacheClear, error) {
 	if _, ok := methods["clearCache"]; !ok {
 		return nil, fmt.Errorf("passed action [%s] is not supported", "clearCache")
 	}
@@ -37,5 +41,31 @@ func (api *API) ClearCache(cacheClear *CacheClear, domainId int) (*CacheClear, e
 	if err != nil {
 		return nil, err
 	}
-	return result.(*CacheClear), nil
+
+	return result.(*[]CacheClear), nil
+}
+
+// decodeCacheClearResponse - custom decode function for cache clear responses
+func decodeCacheClearResponse(resp *http.Response, definition APIMethod) (interface{}, error) {
+	res, err := decodeBaseResponse(resp)
+	if err != nil {
+		return nil, err
+	}
+	result := res.Data
+
+	tmp, err := json.Marshal(result)
+	if err != nil {
+		return nil, err
+	}
+
+	if definition.Result == nil {
+		return tmp, nil
+	}
+
+	decoder := json.NewDecoder(bytes.NewReader(tmp))
+	retValue := reflect.New(reflect.TypeOf(definition.Result))
+	returnResult := retValue.Interface()
+	decoder.Decode(returnResult)
+
+	return returnResult, err
 }
